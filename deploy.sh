0ca47d93ba8cf15dfa8682efad9ee8887bf1929c
JLJ <justin@mnjustin.com>
2024-12-01 12:28:41 -0600

    build and deploy script

---
+++ deploy.sh
@@ -0,0 +1,36 @@
+sudo yum -y remove java-17-amazon-corretto*
+sudo yum install -y https://corretto.aws/downloads/latest/amazon-corretto-11-x64-linux-jdk.rpm
+
+wget https://services.gradle.org/distributions/gradle-6.5-bin.zip -P /tmp
+
+sudo unzip -d /opt/gradle /tmp/gradle-6.5-bin.zip
+
+export GRADLE_HOME=/opt/gradle/gradle-6.5
+export PATH=${GRADLE_HOME}/bin:${PATH}
+
+cat >> ~/.bash_profile  <<TXT
+export GRADLE_HOME=${GRADLE_HOME}
+export PATH=${GRADLE_HOME}/bin:${PATH}
+TXT
+
+# gradle -v   (optional) 
+
+git clone https://github.com/sdjustin1/fuseless.git
+# -------------------- break point #1
+cd fuseless
+
+./test.sh
+# -------------------- break point #2
+cd ..
+
+git clone https://github.com/sdjustin1/sdjustin2.git
+# -------------------- break point #3
+cd sdjustin2
+
+./init.sh
+# -------------------- break point #4
+gradle build
+# -------------------- break point #5
+# sam deploy --guided
+#   or
+sam deploy
